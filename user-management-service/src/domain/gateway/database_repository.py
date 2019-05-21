6ffa66dda09ba3de79f4fcb29eea461abad8a691
Abdulrahman Mazhar <abmazhr@gmail.com>
2019-05-21 17:51:41 +0200

    Add database-repository gateway interface

---
+++ user-management-service/src/domain/gateway/database_repository.py
@@ -0,0 +1,14 @@
+from abc import ABCMeta, abstractmethod
+from typing import Dict, Union
+
+from src.domain.entity.error import Error
+from src.domain.entity.success import Success
+from src.domain.entity.user import User
+
+
+class DatabaseRepository(metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self, configs: Dict[str, str] = None) -> None: pass
+
+    @abstractmethod
+    def save_user(self, *, user: User) -> Union[Error, Success]: pass
