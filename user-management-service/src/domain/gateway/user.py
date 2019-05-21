75b6ef060cafeee7b692e8451675cb8540d073f5
Abdulrahman Mazhar <abmazhr@gmail.com>
2019-05-21 17:50:37 +0200

    Add user gateway interface

---
+++ user-management-service/src/domain/gateway/user.py
@@ -0,0 +1,14 @@
+from abc import ABCMeta, abstractmethod
+from typing import Union
+
+from src.domain.entity.error import Error
+from src.domain.entity.success import Success
+from src.domain.gateway.database_repository import DatabaseRepository
+
+
+class CreateUser(metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self, db_repository: DatabaseRepository) -> None: pass
+
+    @abstractmethod
+    def execute(self) -> Union[Error, Success]: pass
