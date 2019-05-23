d879af2e2325d01715dfab5904542afcccda80cd
Abdulrahman Mazhar <abmazhr@gmail.com>
2019-05-24 00:54:35 +0200

    Update configs file to get mongodb database name as environment variable

--- user-management-service/src/configs.py
+++ user-management-service/src/configs.py
@@ -5,3 +5,4 @@ SERVICE_HOST = getenv('SERVICE_HOST', '0.0.0.0')
 
 MONGODB_HOST = getenv('MONGODB_HOST', 'localhost')
 MONGODB_PORT = int(getenv('MONGODB_PORT', 27017))
+MONGODB_NAME = getenv('MONGODB_NAME', 'users_management')
