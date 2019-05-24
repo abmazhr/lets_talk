4e6e415542202f0dff71d603b96c8e5e821658f9
Abdulrahman Mazhar <abmazhr@gmail.com>
2019-05-24 23:23:10 +0200

    Add build.ts file for building instructions

---
+++ bidirectional-communication-service/build.ts
@@ -0,0 +1,7 @@
+import s from 'shelljs';
+const config = require('./tsconfig.json');
+const outDir = config.compilerOptions.outDir;
+
+s.rm('-rf', outDir);
+s.mkdir(outDir);
+s.cp('.env', `${outDir}/.env`);
