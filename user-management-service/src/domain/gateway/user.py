6de545568e2fee40d1ff033a46b77f753964ca1d
Abdulrahman Mazhar <abmazhr@gmail.com>
2019-05-25 04:36:32 +0200

    Add check-user-credentials gateway interface

--- user-management-service/src/domain/gateway/user.py
+++ user-management-service/src/domain/gateway/user.py
@@ -2,6 +2,7 @@ from abc import ABCMeta, abstractmethod
 from typing import Union, Any, Dict
 
 from src.domain.entity.error import Error
+from src.domain.entity.success import Success
 from src.domain.entity.user import User
 from src.domain.gateway.user_database_repository import UserDatabaseRepository
 
@@ -12,3 +13,11 @@ class CreateUser(metaclass=ABCMeta):
 
     @abstractmethod
     def execute(self, *, data: Dict[str, Any]) -> Union[Error, User]: pass
+
+
+class CheckUserCredentials(metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self, *, db_repository: UserDatabaseRepository) -> None: pass
+
+    @abstractmethod
+    def execute(self, *, data: Dict[str, Any]) -> Union[Error, Success]: pass
