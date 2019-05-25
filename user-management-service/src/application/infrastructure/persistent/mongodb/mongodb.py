d5e386b28dd786c3094b30447ce418f7424e69fe
Abdulrahman Mazhar <abmazhr@gmail.com>
2019-05-25 04:30:46 +0200

    Update mongodb implementation for checking user credentials

--- user-management-service/src/application/infrastructure/persistent/mongodb/mongodb.py
+++ user-management-service/src/application/infrastructure/persistent/mongodb/mongodb.py
@@ -11,7 +11,6 @@ from src.domain.gateway.user_database_repository import UserDatabaseRepository
 
 
 class MongoUserDatabaseRepository(UserDatabaseRepository):
-
     def __init__(self, *, configs: Dict[str, str] = None) -> None:
         super().__init__(configs=configs)
         self.__db = MongoClient(configs.get('MONGODB_HOST'), configs.get('MONGODB_PORT'))[configs.get('MONGODB_NAME')]
@@ -39,6 +38,17 @@ class MongoUserDatabaseRepository(UserDatabaseRepository):
             ))
         except:
             return Error(reason=f'Error saving user {user.name} into database.')
+
+    def check_user_credentials(self, *, username: str, password: str) -> Union[Error, Success]:
+        db_user = self.__models.User.find_one({"name": username})
+        if db_user is not None:
+            db_password = db_user.password
+            if bcrypt.checkpw(password.encode(), db_password.encode()):
+                return Success(data=f"This user's '{username}' credentials are valid")
+
+            return Error(reason=f"This user's '{username}' credentials are not valid")
+        else:
+            return Error(reason=f"User with username '{username}' does not exist")
 
     def __does_exist_before(self, *, user: UserEntity) -> bool:
         return self.__models.User.find_one({"name": user.name}) is not None
