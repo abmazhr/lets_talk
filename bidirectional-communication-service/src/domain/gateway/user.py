27f1dfcc71c09db3e4b75c33f3b4bedd19a5b659
Abdulrahman Mazhar <abmazhr@gmail.com>
2019-05-28 01:14:34 +0200

    Update user usecases to include add and remove online users

--- bidirectional-communication-service/src/domain/gateway/user.py
+++ bidirectional-communication-service/src/domain/gateway/user.py
@@ -3,7 +3,9 @@ from typing import Union
 
 from src.domain.entity.error import Error
 from src.domain.entity.success import Success
+from src.domain.entity.user_persistence import UserPersistence
 from src.domain.gateway.http_client import HttpClient
+from src.domain.gateway.online_users_persistent import OnlineUsersPersistent
 
 
 class CheckUserCredentials(metaclass=ABCMeta):
@@ -12,3 +14,19 @@ class CheckUserCredentials(metaclass=ABCMeta):
 
     @abstractmethod
     def execute(self, *, url: str, username: str, password: str) -> Union[Error, Success]: pass
+
+
+class AddOnlineUser(metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self, *, online_user_persistent: OnlineUsersPersistent) -> None: pass
+
+    @abstractmethod
+    def execute(self, *, user_persistence: UserPersistence) -> Union[Error, Success]: pass
+
+
+class RemoveOnlineUser(metaclass=ABCMeta):
+    @abstractmethod
+    def __init__(self, *, online_user_persistent: OnlineUsersPersistent) -> None: pass
+
+    @abstractmethod
+    def execute(self, *, username: str) -> Union[Error, Success]: pass
