086bb660f3cf0b1fe6011acff3fff1aa935769e6
Abdulrahman Mazhar <abmazhr@gmail.com>
2019-05-28 01:14:56 +0200

    Update socket tests

--- bidirectional-communication-service/tests/infrastucture/web/sockets/flask_sockets_implementation/message/test_send_message.py
+++ bidirectional-communication-service/tests/infrastucture/web/sockets/flask_sockets_implementation/message/test_send_message.py
@@ -2,8 +2,11 @@ from typing import Dict, Any, Union
 
 from flask_socketio import SocketIOTestClient
 
+from src.application.infrastructure.persistent.in_memory import InMemoryOnlineUsersPersistent
 from src.application.infrastructure.web.sockets.implementation.flask_sockets_implementation.io import get_io
+from src.application.usecase.user.add_online_user import AddOnlineUserUseCase
 from src.application.usecase.user.check_creds import CheckUserCredentialsUseCase
+from src.application.usecase.user.remove_online_user import RemoveOnlineUserUseCase
 from src.domain.entity.error import Error
 from src.domain.entity.success import Success
 from src.domain.gateway.http_client import HttpClient, Response
@@ -20,8 +23,17 @@ class FakeHttpClient(HttpClient):
 
 def test_valid_send_message_to_all():
     http_client = FakeHttpClient(configs={'result': Success(data={'valid': True})})
+    online_user_persistent = InMemoryOnlineUsersPersistent()
+
     check_user_creds_usecase = CheckUserCredentialsUseCase(http_client=http_client)
-    api, io = get_io(check_user_creds_usecase=check_user_creds_usecase)
+    add_online_user_usecase = AddOnlineUserUseCase(online_user_persistent=online_user_persistent)
+    remove_online_user_usecase = RemoveOnlineUserUseCase(online_user_persistent=online_user_persistent)
+
+    api, io = get_io(
+        check_user_creds_usecase=check_user_creds_usecase,
+        add_online_user_usecase=add_online_user_usecase,
+        remove_online_user_usecase=remove_online_user_usecase
+    )
     client = SocketIOTestClient(app=api, socketio=io, headers={'username': 'fake', 'password': 'fake'})
     _connected_msg = client.get_received().pop(0)
 
