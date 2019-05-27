5ff5e556338ad147f484b4d7a2edff3f26c9a114
Abdulrahman Mazhar <abmazhr@gmail.com>
2019-05-28 01:12:03 +0200

    Update base-sockets gateway interface

--- bidirectional-communication-service/src/application/infrastructure/web/sockets/gateway/base.py
+++ bidirectional-communication-service/src/application/infrastructure/web/sockets/gateway/base.py
@@ -1,11 +1,16 @@
 from abc import ABCMeta, abstractmethod
 
+from src.application.usecase.user.add_online_user import AddOnlineUserUseCase
 from src.application.usecase.user.check_creds import CheckUserCredentialsUseCase
+from src.application.usecase.user.remove_online_user import RemoveOnlineUserUseCase
 
 
 class BaseSockets(metaclass=ABCMeta):
     @abstractmethod
-    def __init__(self, *, check_user_creds_usecase: CheckUserCredentialsUseCase) -> None: pass
+    def __init__(self, *,
+                 check_user_creds_usecase: CheckUserCredentialsUseCase,
+                 add_online_user_usecase: AddOnlineUserUseCase,
+                 remove_online_user_usecase: RemoveOnlineUserUseCase) -> None: pass
 
     @abstractmethod
     def on_connect(self, *args, **kwargs) -> None: pass
