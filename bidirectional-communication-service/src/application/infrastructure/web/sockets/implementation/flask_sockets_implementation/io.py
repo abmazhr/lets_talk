06b0c74b824810cbb076f4e7d0df2a9bd18e9391
Abdulrahman Mazhar <abmazhr@gmail.com>
2019-05-27 02:56:37 +0200

    Update io events for message receiving

--- bidirectional-communication-service/src/application/infrastructure/web/sockets/implementation/flask_sockets_implementation/io.py
+++ bidirectional-communication-service/src/application/infrastructure/web/sockets/implementation/flask_sockets_implementation/io.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask_socketio import SocketIO
 
 from src.application.infrastructure.web.sockets.implementation.flask_sockets_implementation.base import FlaskBaseSockets
+from src.application.infrastructure.web.sockets.implementation.flask_sockets_implementation.message import \
+    FlaskMessageSockets
 from src.application.usecase.user.check_creds import CheckUserCredentialsUseCase
 
 
@@ -14,5 +16,6 @@ def get_io(*, check_user_creds_usecase: CheckUserCredentialsUseCase) -> Tuple[Fl
     # register (maybe we will do it better later so it doesn't be messy right? :D
     io.on_event('connect', FlaskBaseSockets(check_user_creds_usecase=check_user_creds_usecase).on_connect)
     io.on_event('disconnect', FlaskBaseSockets(check_user_creds_usecase=check_user_creds_usecase).on_disconnect)
+    io.on_event('message', FlaskMessageSockets().on_message)
 
     return api, io
