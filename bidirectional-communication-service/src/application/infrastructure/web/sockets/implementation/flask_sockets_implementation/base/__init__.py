b9582351ce8a883a03ad22da3b7e10aebc99c5ce
Abdulrahman Mazhar <abmazhr@gmail.com>
2019-05-28 01:12:33 +0200

    Update flask base-sockets implementation

--- bidirectional-communication-service/src/application/infrastructure/web/sockets/implementation/flask_sockets_implementation/base/__init__.py
+++ bidirectional-communication-service/src/application/infrastructure/web/sockets/implementation/flask_sockets_implementation/base/__init__.py
@@ -2,16 +2,25 @@ from flask import request
 from flask_socketio import send, disconnect
 
 from src.application.infrastructure.web.sockets.gateway.base import BaseSockets
+from src.application.usecase.user.add_online_user import AddOnlineUserUseCase
 from src.application.usecase.user.check_creds import CheckUserCredentialsUseCase
+from src.application.usecase.user.remove_online_user import RemoveOnlineUserUseCase
 from src.configs import CHECK_USER_CREDENTIALS_ROUTE
 from src.domain.entity.error import Error
 from src.domain.entity.success import Success
+from src.domain.entity.user_persistence import UserPersistence
 
 
 class FlaskBaseSockets(BaseSockets):
-    def __init__(self, *, check_user_creds_usecase: CheckUserCredentialsUseCase) -> None:
-        super().__init__(check_user_creds_usecase=check_user_creds_usecase)
+    def __init__(self, *, check_user_creds_usecase: CheckUserCredentialsUseCase,
+                 add_online_user_usecase: AddOnlineUserUseCase,
+                 remove_online_user_usecase: RemoveOnlineUserUseCase) -> None:
+        super().__init__(check_user_creds_usecase=check_user_creds_usecase,
+                         add_online_user_usecase=add_online_user_usecase,
+                         remove_online_user_usecase=remove_online_user_usecase)
         self.__check_user_creds_usecase = check_user_creds_usecase
+        self.__add_online_user_usecase = add_online_user_usecase
+        self.__remove_online_user_usecase = remove_online_user_usecase
 
     def on_connect(self, *args, **kwargs) -> None:
         sid = request.sid
@@ -30,9 +39,18 @@ class FlaskBaseSockets(BaseSockets):
                 disconnect(sid=sid)
 
             if isinstance(check_creds_result, Success):
-                msg = f'{sid}:: Connected successfully !'
-                print(msg)
-                send(msg)
+                adding_online_user_result = self.__add_online_user_usecase.execute(
+                    user_persistence=UserPersistence(username=username, socket_id=sid)
+                )
+                if isinstance(adding_online_user_result, Error):
+                    msg = adding_online_user_result.reason
+                    send(msg)
+                    disconnect(sid=sid)
+
+                if isinstance(adding_online_user_result, Success):
+                    msg = f'{sid}:: Connected successfully !'
+                    print(msg)
+                    send(msg)
         else:
             msg = f'Invalid credentials data'
             send(msg)
@@ -40,4 +58,14 @@ class FlaskBaseSockets(BaseSockets):
 
     def on_disconnect(self, *args, **kwargs) -> None:
         sid = request.sid
-        print(f'{sid} :: Disconnected successfully !')
+        removing_online_user_result = self.__remove_online_user_usecase.execute(
+            socket_id=sid
+        )
+        if isinstance(removing_online_user_result, Error):
+            msg = removing_online_user_result.reason
+            print(f"{sid} :: Disconnected with error,\n{msg}")
+            disconnect(sid=sid)
+        if isinstance(removing_online_user_result, Success):
+            msg = f'{sid} :: Disconnected successfully !'
+            print(msg)
+            disconnect(sid=sid)
