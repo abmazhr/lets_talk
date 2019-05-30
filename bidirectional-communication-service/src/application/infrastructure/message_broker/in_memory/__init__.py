e909b7ccd52edac6a8f6ae8d2e8fb6b58ee8f607
Abdulrahman Mazhar <abmazhr@gmail.com>
2019-05-30 03:09:47 +0200

    Add initial in-memory message broker implementation

---
+++ bidirectional-communication-service/src/application/infrastructure/message_broker/in_memory/__init__.py
@@ -0,0 +1,17 @@
+from queue import Queue
+from typing import Dict, Any, Union as Either
+
+from src.domain.entity.error import Error
+from src.domain.entity.success import Success
+from src.domain.gateway.message_broker import MessageBroker
+
+
+class InMemoryMessageBroker(MessageBroker):
+    def __init__(self, *, configs: Dict[str, Any]) -> None:
+        super().__init__(configs=configs)
+        self.__broker = Queue()  # in memory ;)
+
+    def publish_message(self, *, topic: str, message: Dict[str, Any]) -> Either[Error, Success]:
+        # since this is in-memory so no need th e  topic :)
+        self.__broker.put(item=message)
+        return Success(data=None)
