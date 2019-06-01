c8a26367638e7e1f36b0c27057d0a1d46a794c7d
Abdulrahman Mazhar <abmazhr@gmail.com>
2019-06-02 01:07:59 +0200

    Add initial utils functionalities for frontend app

---
+++ frontend-cli-app/src/utils/__init__.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from src.handlers.chatting import chatting_handler
+from src.handlers.login import login_handler
+from src.handlers.logout import logout_handler
+from src.handlers.register import register_handler
+
+
+class MenuItems:
+    REGISTER_NEW_USER = ('Register a new user.', register_handler)
+    LOGIN_WITH_EXISTED_USER = ('Login with an existed user.', login_handler)
+    LOGOUT_CURRENT_LOGGED_USER = ('Logout.', logout_handler)
+    CHAT = ('Start chatting.', chatting_handler)
+    EXIT = ('Exit the app.', lambda: exit(0))
+
+
+def get_cli_menu(menu_items: List[str]) -> str:
+    menu_str = """
+###############################################################################
+#                             REAL-TIME CHAT CLIENT                           #
+###############################################################################
+"""
+    for index, item_str in enumerate(menu_items):
+        menu_str += f'{index + 1}) {item_str}\n'
+
+    return menu_str
