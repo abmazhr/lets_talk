ff0100f0571bab09f19fb712bf69ecc07d940131
Abdulrahman Mazhar <abmazhr@gmail.com>
2019-06-02 03:15:32 +0200

    Update the loop mechanism

--- frontend-cli-app/src/loop.py
+++ frontend-cli-app/src/loop.py
@@ -2,6 +2,8 @@ from typing import List, Callable
 
 
 def loop(*, current_menu_handlers: List[Callable]) -> None:
+    from src.utils import get_cli_menu
+
     invalid_choice_msg = 'Error, please choose a valid choice.'
     try:
         choice = int(input("⇝ "))
@@ -9,8 +11,11 @@ def loop(*, current_menu_handlers: List[Callable]) -> None:
             print(invalid_choice_msg)
             loop(current_menu_handlers=current_menu_handlers)
         else:
-            current_menu_handlers[choice - 1]()
-            loop(current_menu_handlers=current_menu_handlers)
+            new_menu_containers: List[Callable] = current_menu_handlers[choice - 1]()
+            new_menu_items_str = list(map(lambda tup: tup[0], new_menu_containers))
+            new_menu_items_handlers = list(map(lambda tup: tup[1], new_menu_containers))
+            print(get_cli_menu(menu_items=new_menu_items_str))
+            loop(current_menu_handlers=new_menu_items_handlers)
     except ValueError:
         print(invalid_choice_msg)
         loop(current_menu_handlers=current_menu_handlers)
